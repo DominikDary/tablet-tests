c6c3940c34786fe37e867f2d510f7804ad8d7767
Dary, Dominik <ddary@ebay.com>
2013-01-30 13:47:18 +0100

    added tests for android-driver

--- README.md
+++ README.md
@@ -5,5 +5,18 @@ Project to play with the Opera Mobile Emulator to test a tablet web site with [O
 
 This is only tested on MAC.
 
+Using Opera Mobile Emulator
+---------------------------
+
 To get started you must install first the Opera Mobile Emulator from the [App Store](https://itunes.apple.com/de/app/opera-mobile-emulator/id515290645?mt=12)
 
+
+Using Android Driver (Selenium)
+-------------------------------
+
+For running the test you need to create an Android Emulator first and then download the [android-server-2.21.0.apk](http://selenium.googlecode.com/files/android-server-2.21.0.apk). After doing this do:
+
+* install the app on your emulator with: *adb install -r  android-server.apk*
+* start the app: *adb shell am start -a android.intent.action.MAIN -n org.openqa.selenium.android.app/.MainActivity*
+* activate the port forwarding: *adb forward tcp:8080 tcp:8080*
+* now you are able to run the tests
--- src/main/java/de/dary/webdriver/tablet/TabletWebTests.java
+++ src/main/java/de/dary/webdriver/tablet/TabletWebTests.java
@@ -5,6 +5,8 @@ import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.android.AndroidDriver;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -13,11 +15,35 @@ import com.opera.core.systems.OperaProduct;
 
 public class TabletWebTests {
   @Test
-  public void checkDailyDealsDE() throws Exception {
+  public void testInOperaMobileBrowser() throws Exception {
     WebDriver driver = new OperaMobileDriver(getNexus7OperaCapabilities());
+    checkDailyDealsDE(driver);
+  }
+
+  @Test
+  public void testInAndroidMobileBrowser() throws Exception {
+    WebDriver driver = new AndroidDriver();
+    checkDailyDealsDE(driver);
+  }
+
+  protected void checkDailyDealsDE(WebDriver driver) throws Exception {
     driver.get("http://deals.ebay.de");
     WebElement header = driver.findElement(By.cssSelector("h1.hb_h1Content"));
     Assert.assertEquals("eBay WOW! Angebote", header.getText());
+    WebElement toys = driver.findElement(By.linkText("Spielzeug"));
+    toys.click();
+    driver.findElement(By.linkText("Kinderspielzeug")).click();
+    WebElement toysHeader = driver.findElement(By.cssSelector("span.sp_mainTitleDiv"));
+    Assert.assertEquals("Spielzeug- WOW! Angebote", toysHeader.getText());
+    driver.findElement(By.cssSelector("input.minPrice")).sendKeys("20");
+
+    driver.findElement(By.cssSelector("input.PriceFilter")).click();
+  }
+
+  private void doubleTapElement(WebElement element, WebDriver driver) {
+    TouchActions action = new TouchActions(driver);
+    action.doubleTap(element);
+    action.build().perform();
   }
 
   private DesiredCapabilities getNexus7OperaCapabilities() {
